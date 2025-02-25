10e77cf60ae0f1ec46d3eb0e6896509778912e53
Adam Brunner <adamino@Adams-MacBook-Pro.local>
2025-02-25 07:40:30 -0700

    adding css and basic structure

---
+++ app.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+
+const mongoose = require('mongoose');
+const db_uri = process.env.DB_URI || 'mongodb://localhost:27017/demo';
+mongoose.connect(db_uri);
+const db = mongoose.connection;
+
+db.on('error', (err) => {
+    console.error(err);
+});
+db.once('open', () => {
+    console.log('Database connected');
+})
+
+const User = mongoose.Schema({
+    firstName: String,
+    lastName: String,
+    email: String,
+    age: Number,
+    createdDate: { type: Date, default: Date.now }
+});
+
+const UserModel = mongoose.model('User', User);
+
+app.listen(port, (err) => {
+    if (err) console.log(err);
+    console.log(`App Server listening on port ${port}`)
+})
+
+app.post('/newUser', (req, res) => {
+    console.log(`POST /newUser: ${JSON.stringify(req.body)}`);
+    const newUser = new User();
+    newUser.firstName = req.body.firstName;
+    newUser.lastName = req.body.lastName;
+    newUser.email = req.body.email;
+    newUser.age = req.body.age;
+    newUser.save((err, data) => {
+        if (err) {
+            return console.error(err);
+        }
+        console.log(`new user save: ${data}`);
+        res.send(`done ${data}`)
+    })
+});
+
+// app.post('/users', async (req, res) => {
+//     const name = req.body.name;
+//     const role = req.body.role;
+//     const age = req.body.age;
+//     try {
+//         const user = new UserModel({ name, role, age });
+//         const result = await user.save();
+//         res.send(result);
+//     } catch (err) {
+//         res.status(500).send(err);
+//      }
+    
+//     const user = new UserModel(req.body);
+    
+//     try {
+//         const result = await user.save();
+//         res.send(result);
+//     } catch (err) {
+//         res.status(500).send
+//     }
+// })
